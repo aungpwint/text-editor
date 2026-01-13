8a40eed269c29fa77921ea17663ee2922ba3d5b6
Aung Pwint <aungpwintofficial@gmail.com>
2026-01-13 13:03:33 +0630

    Extensions Factory

---
+++ components/editor/extensions/index.ts
@@ -0,0 +1,30 @@
+import type { Extensions } from '@tiptap/core'
+import StarterKit from '@tiptap/starter-kit'
+import { EditorConfig } from '../types'
+
+/**
+ * StarterKit
+ */
+export const StarterKitExtension = StarterKit.configure({
+    heading: {
+        levels: [1, 2, 3, 4, 5, 6],
+    },
+    link: {
+        openOnClick: false,
+    },
+    bulletList: false,
+    orderedList: false,
+    listItem: false,
+    listKeymap: false,
+})
+
+/**
+ * Extensions Factory
+ */
+export const getExtensions = (config?: EditorConfig): Extensions => {
+    const extensions: Extensions = [
+        StarterKitExtension,
+    ]
+
+    return extensions
+}
