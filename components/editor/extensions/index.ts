a3db655cc40c69682e910c45e82d3010eda2a9d7
Aung Pwint <aungpwintofficial@gmail.com>
2026-02-10 23:19:41 +0630

    getExtensions

--- components/editor/extensions/index.ts
+++ components/editor/extensions/index.ts
@@ -1,7 +1,12 @@
 import type { Extensions } from '@tiptap/core'
 import StarterKit from '@tiptap/starter-kit'
+
+import { ListKit } from '@tiptap/extension-list'
+import { Subscript } from '@tiptap/extension-subscript'
 import { Superscript } from '@tiptap/extension-superscript'
+import { TextAlign } from '@tiptap/extension-text-align'
 import { FontFamily, LineHeight, TextStyle } from '@tiptap/extension-text-style'
+
 import { EditorConfig } from '../types'
 
 /**
@@ -23,18 +28,38 @@ export const StarterKitExtension = StarterKit.configure({
 /**
  * Extensions Factory
  */
-export const getExtensions = (config?: EditorConfig): Extensions => {
+export const getExtensions = (config?: EditorConfig, EditorFloatingMenu?: any): Extensions => {
     const extensions: Extensions = [
         StarterKitExtension,
-        Superscript,
         TextStyle,
+        Subscript,
+        Superscript,
         LineHeight.configure({
             types: ['textStyle'],
         }),
         FontFamily.configure({
             types: ['textStyle'],
         }),
+        TextAlign.configure({
+            types: ['heading', 'paragraph'],
+        }),
+        ListKit.configure({
+            listItem: {
+                HTMLAttributes: { class: 'list-item' },
+            },
+            taskList: {
+                HTMLAttributes: { class: 'task-list' },
+            },
+            taskItem: {
+                HTMLAttributes: { class: 'task-item' },
+                nested: false,
+            },
+        }),
     ]
+
+    if (config?.enableFloatingMenu && EditorFloatingMenu) {
+        extensions.push(EditorFloatingMenu)
+    }
 
     return extensions
 }
